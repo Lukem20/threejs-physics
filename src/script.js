c41a5d3f3db67a768ca50c9a1e6ca00a675dafba
Luke Moore <lukemoore237@gmail.com>
2024-01-26 09:48:20 -0800

    Adding createSphere method to add many spheres with physics.

--- src/script.js
+++ src/script.js
@@ -18,12 +18,12 @@ import GUI from 'lil-gui';
 import CANNON from 'cannon';
 
 /**
- * Debug
+ * Debug panel
  */
 const gui = new GUI();
 
 /**
- * Base
+ * Base scene
  */
 const canvas = document.querySelector('canvas.webgl');
 const scene = new Scene();
@@ -33,7 +33,6 @@ const scene = new Scene();
  */
 const textureLoader = new TextureLoader();
 const cubeTextureLoader = new CubeTextureLoader();
-
 const environmentMapTexture = cubeTextureLoader.load([
     '/textures/environmentMaps/0/px.png',
     '/textures/environmentMaps/0/nx.png',
@@ -43,21 +42,6 @@ const environmentMapTexture = cubeTextureLoader.load([
     '/textures/environmentMaps/0/nz.png'
 ]);
 
-/**
- * Test sphere
- */
-const sphere = new Mesh(
-    new SphereGeometry(0.5, 32, 32),
-    new MeshStandardMaterial({
-        metalness: 0.3,
-        roughness: 0.4,
-        envMap: environmentMapTexture,
-        envMapIntensity: 0.5
-    })
-);
-sphere.castShadow = true;
-sphere.position.y = 0.5;
-scene.add(sphere);
 
 /**
  * Floor
@@ -74,15 +58,15 @@ const floor = new Mesh(
 );
 floor.receiveShadow = true;
 floor.rotation.x = - Math.PI * 0.5;
+
 scene.add(floor);
 
 /**
  * Lights
  */
 const ambientLight = new AmbientLight(0xffffff, 2.1);
-scene.add(ambientLight);
-
 const directionalLight = new DirectionalLight(0xffffff, 0.6);
+
 directionalLight.castShadow = true;
 directionalLight.shadow.mapSize.set(1024, 1024);
 directionalLight.shadow.camera.far = 15;
@@ -91,10 +75,11 @@ directionalLight.shadow.camera.top = 7;
 directionalLight.shadow.camera.right = 7;
 directionalLight.shadow.camera.bottom = - 7;
 directionalLight.position.set(5, 5, 5);
-scene.add(directionalLight);
+
+scene.add(ambientLight, directionalLight);
 
 /**
- * Sizes
+ * Screen sizes
  */
 const sizes = {
     width: window.innerWidth,
@@ -102,11 +87,11 @@ const sizes = {
 };
 
 window.addEventListener('resize', () => {
-    // Update sizes
+    // Update size values on screen resize
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
 
-    // Update camera
+    // Update camera projection
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
 
@@ -118,49 +103,41 @@ window.addEventListener('resize', () => {
 /**
  * Physics
  */
+// Start with creating a "physics world" representation
 const world = new CANNON.World();
 world.gravity.set(0, -9.82, 0);
 
-const concreteMaterial = new CANNON.Material('concrete');
-const plasticMaterial = new CANNON.Material('plastic');
-const contactConcretePlastic = new CANNON.ContactMaterial(
-    concreteMaterial,
-    plasticMaterial,
+// Create contact materials for objects with physics
+const defaultMaterial = new CANNON.Material('default');
+const defaultContactMaterial = new CANNON.ContactMaterial(
+    defaultMaterial,
+    defaultMaterial,
     {
         friction: 0.1,
         restitution: 0.7,
     }
 );
-world.addContactMaterial(contactConcretePlastic);
 
-const sphereShape = new CANNON.Sphere(0.5);
-const sphereBody = new CANNON.Body({
-    mass: 1,
-    position: new CANNON.Vec3(0, 3, 0),
-    shape: sphereShape,
-    material: plasticMaterial,
-});
+world.addContactMaterial(defaultContactMaterial);
+world.defaultContactMaterial = defaultContactMaterial;
 
+// Floor contact physics
 const floorShape = new CANNON.Plane();
 const floorBody = new CANNON.Body();
 floorBody.mass = 0;
-floorBody.material = concreteMaterial;
+floorBody.material = defaultMaterial;
 floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5);
 floorBody.addShape(floorShape);
 
 world.addBody(floorBody);
-world.addBody(sphereBody);
-
 
 /**
  * Camera
  */
-// Base camera
 const camera = new PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
 camera.position.set(- 3, 3, 3);
 scene.add(camera);
 
-// Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
@@ -176,6 +153,47 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
+ * Utils
+ */
+const objectsToUpdate = [];
+
+const createSphere = (radius, position) => {
+    const mesh = new Mesh(
+        new SphereGeometry(radius, 20, 20),
+        new MeshStandardMaterial({
+            metalness: 0.3,
+            roughness: 0.4,
+            envMap: environmentMapTexture
+        })
+    );
+
+    mesh.castShadow = true;
+    mesh.position.copy(position);
+    scene.add(mesh);
+
+    const shape = new CANNON.Sphere(radius);
+    const body = new CANNON.Body({
+        mass: 1,
+        position: new CANNON.Vec3( 0 , 3, 0),
+        shape,
+        material: defaultMaterial
+    });
+
+    body.position.copy(position);
+    world.addBody(body);
+
+    // Save object to update
+    objectsToUpdate.push({
+        mesh,
+        body,
+    });
+}
+createSphere(0.5, { x: 0, y: 3, z: 0 });
+createSphere(0.5, { x: 2, y: 3, z: 0 });
+createSphere(0.5, { x: 0, y: 3, z: 2 });
+
+
+/**
  * Animate
  */
 const clock = new Clock();
@@ -189,14 +207,12 @@ const tick = () => {
 
     // Update physics world for 60fps
     world.step(1 / 60, deltaTime, 3)
-    
-    sphere.position.copy(sphereBody.position);
 
+    for(const object of objectsToUpdate) {
+        object.mesh.position.copy(object.body.position);
+    }
 
-    // Update controls
     controls.update();
-
-    // Render what the camera is currently looking at
     renderer.render(scene, camera);
 
     // Call tick again on the next frame
